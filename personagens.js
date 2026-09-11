cd86350361bd40a7f02e2779066004a99ab290c0
JuanSilva310 <profetadiario2019ete@gmail.com>
2026-09-11 19:25:54 -0300

    Add files via upload

---
+++ personagens.js
@@ -0,0 +1,36 @@
+const characters = {
+
+  yami_yugi: {
+    id: "yami_yugi",
+    name: "Yami Yugi",
+
+    characterKey: "CK-YAMI-YUGI-0001",
+
+    alterEgo: true,
+    alterEgoId: "00001121",
+    switchId: "yugi_muto"
+  },
+
+  seto_kaiba: {
+    id: "seto_kaiba",
+    name: "Seto Kaiba",
+
+    characterKey: "CK-SETO-KAIBA-0002",
+
+    alterEgo: false,
+    alterEgoId: null,
+    switchId: null
+  },
+
+  yugi_muto: {
+    id: "yugi_muto",
+    name: "Yugi Muto",
+
+    characterKey: "CK-YUGI-MUTO-0003",
+
+    alterEgo: true,
+    alterEgoId: "00001121",
+    switchId: "yami_yugi"
+  }
+
+};
